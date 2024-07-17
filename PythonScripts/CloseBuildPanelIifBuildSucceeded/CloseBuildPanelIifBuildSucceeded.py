4abed392cb70c21b927850b9c0f816f967b14178
Stewart Lynch <slynch@puredevsoftware.com>
2024-07-17 23:16:16 +0100

    Delete CloseBuildPanelIifBuildSucceeded.py

--- PythonScripts/CloseBuildPanelIifBuildSucceeded/CloseBuildPanelIifBuildSucceeded.py
+++
@@ -1,8 +0,0 @@
-import N10X
-
-# Close build panel if build succeeded
-def OnBuildFinished(build_result):
-    if build_result:
-        N10X.Editor.ExecuteCommand("CloseBuildPanel")
-
-N10X.Editor.AddBuildFinishedFunction(OnBuildFinished)
