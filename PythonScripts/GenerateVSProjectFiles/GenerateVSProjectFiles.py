0e96f07fc05170e5593b7d7b7cae153c048cec5b
Cameron Skinner <cameron@arcanacom.com>
2025-09-05 15:04:52 -0600

    Create a script that allows the 10x user to generate the Visual Studio solution and project files that back the Visual Studio Workspace in 10x

---
+++ PythonScripts/GenerateVSProjectFiles/GenerateVSProjectFiles.py
@@ -0,0 +1,40 @@
+#------------------------------------------------------------------------
+import N10X
+import os
+from datetime import datetime
+
+"""
+In order for this method to succeed, the following setting values need to be placed in the Settings.10x_settings file:
+
+UE.Project:                         <Absolute path to uproject, using \ for path delimiters. EG: E:\Unreal\MyProj\MyProj.uproject>
+UE.InstallDir:                      <Absolute path to UnrealEngine install. EG: D:\UnrealEngine>
+
+# Use the value specified here for the UE.GenVSProjCommand. So just copy the following line and put directly into your settings file:
+UE.GenVSProjCommand:                dotnet "{ueInstallDir}\Engine\Binaries\DotNET\UnrealBuildTool\UnrealBuildTool.dll" -projectfiles -project="{ueProject}" -game -engine -progress -log="{logPath}"
+
+Once these settings are in place, create a new keystroke in your key map. For example:
+Alt Shift Control G:        GenerateVSProjectFiles()
+"""
+def GenerateVSProjectFiles():
+    N10X.Editor.LogTo10XOutput("\n\n[UE] Generate Visual Studio Project\n")
+
+    ueProject = N10X.Editor.GetSetting("UE.Project")
+    ueInstallDir = N10X.Editor.GetSetting("UE.InstallDir")
+    ueCommand = N10X.Editor.GetSetting("UE.GenVSProjCommand")
+
+
+    index = ueProject.rindex("\\")
+    if index == -1:
+        index = ueProject.rindex("/")
+
+    slice = len(ueProject) - index
+    currentDateTime = datetime.now()
+    timeStr = currentDateTime.strftime("%Y.%m.%d-%H.%M.%S")
+    logPath = ueProject[:-slice] + f"\\Saved\\Logs\\10x-{timeStr}.log"      
+
+    fullCommand = ueCommand.format( ueProject = ueProject, ueInstallDir = ueInstallDir, logPath = logPath)
+    N10X.Editor.LogTo10XOutput(f"[UE] ueProject: {ueProject}\n[UE] ueInstallDir: {ueInstallDir}\n[UE] logPath: {logPath}\n[UE] ueCommand: {ueCommand}\n")
+    N10X.Editor.LogTo10XOutput("[UE] Full Command:" + fullCommand + "\n")
+
+    os.system(fullCommand)
+
