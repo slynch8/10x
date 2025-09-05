8df56061a224b53af9444ad16a8aa06f498cb255
Cameron Skinner <cameron@arcanacom.com>
2025-09-05 15:17:18 -0600

    Fixed a comment that prevent the script from running

--- PythonScripts/GenerateVSProjectFiles/GenerateVSProjectFiles.py
+++ PythonScripts/GenerateVSProjectFiles/GenerateVSProjectFiles.py
@@ -3,18 +3,18 @@ import N10X
 import os
 from datetime import datetime
 
-"""
-In order for this method to succeed, the following setting values need to be placed in the Settings.10x_settings file:
 
-UE.Project:                         <Absolute path to uproject, using \ for path delimiters. EG: E:\Unreal\MyProj\MyProj.uproject>
-UE.InstallDir:                      <Absolute path to UnrealEngine install. EG: D:\UnrealEngine>
+# In order for this method to succeed, the following setting values need to be placed in the Settings.10x_settings file:
 
-# Use the value specified here for the UE.GenVSProjCommand. So just copy the following line and put directly into your settings file:
-UE.GenVSProjCommand:                dotnet "{ueInstallDir}\Engine\Binaries\DotNET\UnrealBuildTool\UnrealBuildTool.dll" -projectfiles -project="{ueProject}" -game -engine -progress -log="{logPath}"
+# UE.Project:                         <Absolute path to uproject, using \ for path delimiters. EG: E:\Unreal\MyProj\MyProj.uproject>
+# UE.InstallDir:                      <Absolute path to UnrealEngine install. EG: D:\UnrealEngine>
 
-Once these settings are in place, create a new keystroke in your key map. For example:
-Alt Shift Control G:        GenerateVSProjectFiles()
-"""
+# # Use the value specified here for the UE.GenVSProjCommand. So just copy the following line and put directly into your settings file:
+# UE.GenVSProjCommand:                dotnet "{ueInstallDir}\Engine\Binaries\DotNET\UnrealBuildTool\UnrealBuildTool.dll" -projectfiles -project="{ueProject}" -game -engine -progress -log="{logPath}"
+
+# Once these settings are in place, create a new keystroke in your key map. For example:
+# Alt Shift Control G:        GenerateVSProjectFiles()
+
 def GenerateVSProjectFiles():
     N10X.Editor.LogTo10XOutput("\n\n[UE] Generate Visual Studio Project\n")
 
@@ -37,4 +37,3 @@ def GenerateVSProjectFiles():
     N10X.Editor.LogTo10XOutput("[UE] Full Command:" + fullCommand + "\n")
 
     os.system(fullCommand)
-
