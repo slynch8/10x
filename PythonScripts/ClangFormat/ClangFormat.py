8f0ee41dd38503ee39942a94940dc6f355279d8f
Nicolas <nicolas@uucidl.com>
2026-04-01 14:19:59 +0900

    ClangFormat: Add Post Save Hook
    This save hook will reformat files on save. It is disabled by default,
    you must enable it with `ClangFormat.OnSave: true`
    
    ClangFormat: Suppress console window.
    
    We pass subprocess.CREATE_NO_WINDOW to prevent a console window from
    flashing and interfering with 10x (otherwise might prevent it from
    seeing the release event of the Ctrl key, when saving with Ctrl-S)

--- PythonScripts/ClangFormat/ClangFormat.py
+++ PythonScripts/ClangFormat/ClangFormat.py
@@ -3,6 +3,8 @@
 #     - "ClangFormat.Path": Path to clang-format executable, default: "clang-format.exe"
 #     - "ClangFormat.Style": One of 'LLVM', 'GNU', 'Google', 'Chromium', 'Microsoft', 'Mozilla', 'WebKit' values
 #                            Or 'file' if you are providing .clang-format file in your project
+#     - "ClangFormat.OnSave": 'true' or 'false'. When true, clang-format is called on save
+#     - "ClangFormat.OnSaveExtensions": comma-separated list of extensions allowed to auto-format on save
 #
 import subprocess
 import os
@@ -50,7 +52,8 @@ def _ClangFormat(file, line_range=None):
 
         process = subprocess.Popen(command,
                             shell=True, stdin=None, stdout=None, stderr=subprocess.PIPE,
-                            close_fds=True, cwd=cwd)
+                            close_fds=True, cwd=cwd,
+                            creationflags=subprocess.CREATE_NO_WINDOW)
         _, stderr = process.communicate()
         if process.returncode != 0:
             print('[ClangFormat]: clang-format failed (exit code ' + str(process.returncode) + ')')
@@ -72,3 +75,26 @@ def ClangFormatSelection():
 def ClangFormatFile():
     N10X.Editor.SaveFile()
     _ClangFormat(N10X.Editor.GetCurrentFilename())
+
+
+#------------------------------------------------------------------------
+def ClangFormatPostSave(file):
+    if N10X.Editor.GetSetting("ClangFormat.OnSave") != "true":
+        return
+
+    EXTS = [
+        ".c", ".cc", ".cpp", ".c++", ".cp", ".cxx",
+        ".h", ".hh", ".hpp", ".h++",".hp",".hxx",
+        ".inl",".ixx"
+    ]
+    extensions = N10X.Editor.GetSetting("ClangFormat.OnSaveExtensions")
+    if not extensions:
+        extensions = EXTS
+    else:
+        extensions = [e.strip() for e in extensions.split(",")]
+
+    if any(file.endswith(ext) for ext in extensions):
+        _ClangFormat(file)
+
+
+N10X.Editor.AddPostFileSaveFunction(ClangFormatPostSave)
